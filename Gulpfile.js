71cd8f7ab1c212908d5e64b6e1457bc98d091bd9
MTTHWBSH <bushmat4@gmail.com>
2015-03-26 17:19:05 -0500

    please send help

---
+++ Gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp'),
+	imagemin = require('gulp-imagemin'),
+	pngquant = require('imagemin-pngquant'),
+	jpegtran = require('imagemin-jpegtran'),
+	gifsicle = require('imagemin-gifsicle'),
+	optipng = require('imagemin-optipng');
+
+gulp.task('default', function() {
+
+});
